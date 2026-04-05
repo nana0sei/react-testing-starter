768718eb7c7e5e1fe4be82e303ada96d0c8567e1
nana0sei <frimpongnanao@gmail.com>
2026-04-05 08:56:13 +0000

    testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,20 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Nana" />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/nana/i);
+  });
+
+  it("should render login button if name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
